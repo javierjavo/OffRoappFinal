21d0c4e4c1926a72e76996d7d1744b62a6952fa4
Javier Guerrero <javierjavo@live.com.mx>
2017-12-14 21:38:16 -0600

    actualizacion de mensajes
    alertas visuales de los mensajes no leidos (falta completar con db local)

--- src/pages/grupos/grupos.ts
+++ src/pages/grupos/grupos.ts
@@ -3,6 +3,9 @@ import { NavController, NavParams, IonicPage } from 'ionic-angular';
 import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { AngularFireAuth } from 'angularfire2/auth';
 
+/*
+  MSGS: detalles con la mensajeria en db local
+*/ 
 
 interface Icodes{
   semilla:string;
@@ -12,6 +15,7 @@ interface Ichats{
   semilla:string;
   url:string;
   name:string;
+  msgs:number;
 }
 
 @IonicPage()
@@ -27,11 +31,20 @@ export class GruposPage {
   lichatcolection:AngularFirestoreCollection<Ichats>;
   lichats:Ichats[]=[];
 
-  constructor(private afAuth: AngularFireAuth, private db: AngularFirestore, public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private afAuth: AngularFireAuth, private db: AngularFirestore, 
+    public navCtrl: NavController, public navParams: NavParams) {
 
   }
 
-  ionViewDidEnter(){ //obtengo los chats de cada persona pero los debe consultar de otra parte en consulta estatica
+  ionViewDidLoad(){ //obtengo los chats de cada persona pero los debe consultar de otra parte en consulta estatica
+    this.reloadChangesChat();
+    /* MSGS
+    hay que actualizar la db local aqui, guardando los codigos de chat y poniendo los eventos
+    de escucha en el app component ya que ahí mantendra de manera glogal en control de si ya leyo o no
+    los mensajes entrantes*/
+  }
+
+  reloadChangesChat(){
     this.codeschatcolection = this.db.collection('ListaChats').doc(this.afAuth.auth.currentUser.email).collection("codes");
     this.codeschatcolection.snapshotChanges().subscribe(chatList => {
       this.lichats = [];
@@ -47,18 +60,32 @@ export class GruposPage {
             let auxli:Ichats={
               semilla:li_item.payload.doc.data().semilla,
               url:li_item.payload.doc.data().url,
-              name:li_item.payload.doc.data().name
+              name:li_item.payload.doc.data().name,
+              msgs:-1 // MSGS aqui se recupera la lista de la db local de los mensajes leidos o no
             }
             return auxli;
             //this.lichats.push(auxli);
           }));
           //console.log(it);
-          /*falta revicion si el item ya lo contiene 
-          (usando la semilla como id) eliminar el anterior y agregar el nuevo para evitar
-          el bug de doble chat cuando se actualiza*/
-          this.lichats = it.concat(this.lichats);
+          
+          it.forEach(y=>{
+            this.lichats.forEach(x=>{
+              if(x.semilla == y.semilla){
+                y.msgs = x.msgs;// MSGS la asignacion es en tiempo de ejecucion pero tiene que ser local tambien
+                this.lichats.splice(this.lichats.indexOf(x),1);
+              }
+            });
+            this.lichats.push(y);
+          });
         });
-        
+        let messageadvisor:AngularFirestoreCollection<any> = this.db.collection('chats').doc(item.semilla).collection("messages");
+        messageadvisor.snapshotChanges(['added']).subscribe(messageadvisor=>{
+          this.lichats.forEach(x=>{
+            if(x.semilla == item.semilla){
+              x.msgs = x.msgs+1;
+            }
+          });
+        });
       });
     });
   }
@@ -68,6 +95,12 @@ export class GruposPage {
   }
 
   go_chat(item){
+    this.lichats.forEach(x=>{
+      if(x.semilla == item.semilla){
+        x.msgs = 0;
+        //MSGS aqui borramos la info de los mensajes de la db local
+      }
+    });
     this.navCtrl.push('ChatPage',{semilla:item.semilla,name:item.name});
   }
 
